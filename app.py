a3c943d7aa901ea75cc1814c822b83516045ba92
bradyoo12 <yoohoony@hotmail.com>
2024-02-28 22:43:42 +0900

    br

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def hello_world():
     """Example Hello World route."""
     return f"Hello World!"
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
