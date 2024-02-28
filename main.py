8c092a2b69b623923aee6c3fa68121805903e423
bradyoo12 <yoohoony@hotmail.com>
2024-02-29 00:23:53 +0900

    https://github.com/GoogleCloudPlatform/python-docs-samples/tree/050a57b5c43c6b7f926eb112c1c974a143b17f48/run/helloworld

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # https://www.youtube.com/watch?v=zW86dgr85NE
+# https://github.com/GoogleCloudPlatform/python-docs-samples/tree/050a57b5c43c6b7f926eb112c1c974a143b17f48/run/helloworld
 import os
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    return f"Hello World!"
-
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
 
 if __name__ == "__main__":
