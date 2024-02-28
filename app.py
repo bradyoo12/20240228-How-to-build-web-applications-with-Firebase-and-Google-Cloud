0960cdcf1f7d05980313b7a63c22b7aa72a0771b
bradyoo12 <yoohoony@hotmail.com>
2024-02-28 22:33:28 +0900

    remove lines

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+# https://www.youtube.com/watch?v=zW86dgr85NE
 import os
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -9,8 +9,6 @@ app = Flask(__name__)
 def hello_world():
     """Example Hello World route."""
     return f"Hello World!"
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
